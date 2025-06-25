0e6bc6263495d844cefcb313287715ee03dc7f2f
Orest Yakymiv <akimivorest@gmail.com>
2025-06-25 16:34:55 +0300

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
 module "resource_group_storage" {
   source  = "app.terraform.io/mate_Or4ik/resource_group_storage/azurerm"
   version = "1.0.0"
